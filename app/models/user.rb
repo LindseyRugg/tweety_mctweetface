0741589b1fc05131fec6ffcdec0c236df4439dbc
Lindsey Rugg <atlasshrugged17@gmail.com>
2018-07-05 10:32:54 -0700

    navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, presence: true, uniqueness: true
+
   has_many :tweets
 
   mount_uploader :avatar, AvatarUploader
-  
+
 end
