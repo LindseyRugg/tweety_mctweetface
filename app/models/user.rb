5654130e79d699e5f68b00dfbc533dfde00fd290
Lindsey Rugg <atlasshrugged17@gmail.com>
2018-07-10 10:34:27 -0700

    tags links

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:usename]
 
   validates :username, presence: true, uniqueness: true
 
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
 
   mount_uploader :avatar, AvatarUploader
 
